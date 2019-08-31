2c8519ec5ab587dcd0da879e6a6f4694a7a6f5c7
eharshosh <eharshosh@gmail.com>
2019-08-31 11:41:39 +0300

    better names and style

--- main/main.ino
+++ main/main.ino
@@ -1,45 +1,43 @@
-const int controlPin = 3;
-const unsigned long second = 1000;
-const unsigned long minute = second * 60;
-const unsigned long hour = minute * 60;
+const int MOTOR_CONTROL_PIN = 3;
+const unsigned long SECOND = 1000;
+const unsigned long MINUTE = SECOND * 60;
+const unsigned long HOUR = MINUTE * 60;
+
+unsigned long previousClockMotor = 0;
+const unsigned long MOTOR_ON_DURATION = 45 * SECOND;
+const unsigned long MOTOR_ON_INTERVAL = 2 * HOUR - MOTOR_ON_DURATION;
 
 int ledState = LOW;
-
-unsigned long previousMillis = 0;
-unsigned long onDuration = 45 * second;
-unsigned long onInterval = 2 * hour - onDuration;
-
-unsigned long previousMillisLed = 0;
-unsigned long ledOnOffInterval = second;
+unsigned long previousClockLed = 0;
+const unsigned long LED_ON_OFF_INTERVAL = SECOND;
 
 void setup() {
-  pinMode(controlPin, OUTPUT);
+  pinMode(MOTOR_CONTROL_PIN, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(controlPin, HIGH);
-  delay(3 * second);
-  digitalWrite(controlPin, LOW);
+  digitalWrite(MOTOR_CONTROL_PIN, HIGH);
+  delay(3 * SECOND);
+  digitalWrite(MOTOR_CONTROL_PIN, LOW);
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillisLed >= ledOnOffInterval) {
-    // save the last time you blinked the LED
-    previousMillisLed = currentMillis;
+  unsigned long currentClock = millis();
+  if (currentClock - previousClockLed >= LED_ON_OFF_INTERVAL) {    
     // if the LED is off turn it on and vice-versa:
     if (ledState == LOW) {
       ledState = HIGH;
     } else {
       ledState = LOW;
     }
-
     // set the LED with the ledState of the variable:
     digitalWrite(LED_BUILTIN, ledState);
+    // save the last time you blinked the LED
+    previousClockLed = currentClock;
   }
-  if (currentMillis - previousMillis >= onInterval) {
-    digitalWrite(controlPin, HIGH);
+  if (currentClock - previousClockMotor >= MOTOR_ON_INTERVAL) {
+    digitalWrite(MOTOR_CONTROL_PIN, HIGH);
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(onDuration);
-    previousMillis = millis();
+    delay(MOTOR_ON_DURATION);
+    digitalWrite(MOTOR_CONTROL_PIN, LOW);
+    previousClockMotor = millis();
   }
-  digitalWrite(controlPin, LOW);
 }
