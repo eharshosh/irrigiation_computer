2aaa29af5955e0e01c6a79c126299275e4c96a14
eharshosh <eharshosh@gmail.com>
2019-09-06 12:35:22 +0300

    simplified logic

--- main/main.ino
+++ main/main.ino
@@ -3,41 +3,48 @@ const unsigned long SECOND = 1000;
 const unsigned long MINUTE = SECOND * 60;
 const unsigned long HOUR = MINUTE * 60;
 
-unsigned long previousClockMotor = 0;
 const unsigned long MOTOR_ON_DURATION = 45 * SECOND;
 const unsigned long MOTOR_ON_INTERVAL = 2 * HOUR - MOTOR_ON_DURATION;
+unsigned long nextMotorToggleTime = 0;
 
+int motorState = LOW;
 int ledState = LOW;
-unsigned long previousClockLed = 0;
+
+unsigned long nextLedToggleTime = 0;
 const unsigned long LED_ON_OFF_INTERVAL = SECOND;
 
 void setup() {
   pinMode(MOTOR_CONTROL_PIN, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(MOTOR_CONTROL_PIN, HIGH);
-  delay(3 * SECOND);
-  digitalWrite(MOTOR_CONTROL_PIN, LOW);
+}
+
+void toggleLed(unsigned long currentClock) {
+  if (ledState == LOW) {
+    ledState = HIGH;
+  } else {
+    ledState = LOW;
+  }
+  digitalWrite(LED_BUILTIN, ledState);
+  nextLedToggleTime = currentClock + LED_ON_OFF_INTERVAL;
+}
+
+void toggleMotor(unsigned long currentClock) {
+  if (motorState == LOW) {
+    motorState = HIGH;
+    nextMotorToggleTime = currentClock + MOTOR_ON_DURATION;
+  } else {
+    motorState = LOW;
+    nextMotorToggleTime = currentClock + MOTOR_ON_INTERVAL;
+  }
+  digitalWrite(MOTOR_CONTROL_PIN, motorState);    
 }
 
 void loop() {
   unsigned long currentClock = millis();
-  if (currentClock - previousClockLed >= LED_ON_OFF_INTERVAL) {    
-    // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }
-    // set the LED with the ledState of the variable:
-    digitalWrite(LED_BUILTIN, ledState);
-    // save the last time you blinked the LED
-    previousClockLed = currentClock;
+  if (currentClock >= nextLedToggleTime) {    
+    toggleLed(currentClock);
   }
-  if (currentClock - previousClockMotor >= MOTOR_ON_INTERVAL) {
-    digitalWrite(MOTOR_CONTROL_PIN, HIGH);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(MOTOR_ON_DURATION);
-    digitalWrite(MOTOR_CONTROL_PIN, LOW);
-    previousClockMotor = millis();
+  if (currentClock >= nextMotorToggleTime) {
+    toggleMotor(currentClock);
   }
 }
